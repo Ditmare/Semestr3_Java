a9af7dd97643bebb737c8a7549f87b95757a7d4f
Ditmare <144803026+Ditmare@users.noreply.github.com>
2024-10-28 11:48:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,22 @@ public class Main {
 
         Car car1 = new Car("легковой", "ВАЗ 2107", "Лада");
         Car car2 = new Car("грузовой", "43118", "КАМАЗ");
+        Car car3 = new Car("грузовой", "Prius", "Toyota");
+
         showroom.addCar(car1);
         showroom.addCar(car2);
+        showroom.addCar(car3);
 
         showroom.showAllCars();
 
-        showroom.showSales();
+        Customer customer1 = new Customer("Иванов Иван Иванович", 26, "Мужской");
+        Customer customer2 = new Customer("Иванова Анна Ивановга", 25, "Женский");
 
+        showroom.sellCar(car1, customer1);
+        showroom.sellCar(car3, customer2);
+
+        showroom.showSales();
+        
         showroom.showAllCars();
     }
 }
